a98dd25d684f805d174baa410fccce57126df603
jadengohzh <jadengoh1999@gmail.com>
2023-10-04 11:52:06 +0800

    Updated testdata.sql: added portfolio test data

--- Database/Testdata.sql
+++ Database/Testdata.sql
@@ -26,3 +26,13 @@ VALUES ('2023-10-03 09:00:00', 50.00, 100, 1, 'AAPL');
 -- Insert another Trade for User 1's Portfolio
 INSERT INTO trades (purchase_date_time, purchase_price, purchase_quantity, portfolio_id, stock_symbol)
 VALUES ('2023-10-04 10:30:00', 55.00, 75, 1, 'GOOGL');
+
+-- Insert Test Portfolios for User 1
+INSERT INTO portfolios (portfolio_id, portfolio_name, description, capital, user_id) 
+VALUES (1, 'Tech Stocks', 'Investment in technology sector', 50000.00, 1);
+
+
+INSERT INTO portfolios (portfolio_id, portfolio_name, description, capital, user_id) 
+VALUES (2, 'Real Estate', 'Real estate investments', 75000.00, 1);
+
+
