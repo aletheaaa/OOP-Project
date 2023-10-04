1dac9924df70db2e5caf890ea5eef0c3022b660e
Hoseph132 <joseph.ho1996@gmail.com>
2023-10-04 13:04:59 +0800

    Created getTradesByPorfolioByID

--- Backend/src/main/java/is442/portfolioAnalyzer/Trade/TradeController.java
+++ Backend/src/main/java/is442/portfolioAnalyzer/Trade/TradeController.java
@@ -2,6 +2,7 @@ package is442.portfolioAnalyzer.Trade;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +19,12 @@ public class TradeController {
     public List<Trade> getAllTrades(){
         System.out.println("In controller");
         return tradeService.getAllTrades();
+    }
+
+
+    @GetMapping("getTradesByPortfolioId/{portfolioId}")
+    public List<Trade> getTradesByPortfolioId(@PathVariable Integer portfolioId){
+        System.out.println("In controller");
+        return tradeService.getTradesByPortfolioId(portfolioId);
     }
 }
