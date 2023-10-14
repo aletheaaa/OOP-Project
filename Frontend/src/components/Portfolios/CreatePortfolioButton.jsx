96493af582b8871916c80384d56d5f933e6272b5
Alethea Toh <aletheatohh@gmail.com>
2023-10-14 13:28:33 +0800

    Updated create portfolio form

--- Frontend/src/components/Portfolios/CreatePortfolioButton.jsx
+++ Frontend/src/components/Portfolios/CreatePortfolioButton.jsx
@@ -3,10 +3,44 @@ import React, { useEffect, useState } from "react";
 export default function CreatePortfolioButton() {
   const [validStocks, setValidStocks] = useState([]);
   const [stock, setStock] = useState("");
+  const [numStockFields, setNumStockFields] = useState(1);
+  const [chosenStockAllocation, setChosenStockAllocation] = useState([
+    { stock: "", allocation: 0, id: 0 },
+  ]);
+  const [totalStockAlloc, setTotalStockAlloc] = useState(0);
 
   useEffect(() => {
     setValidStocks(["AAPL", "TSLA"]);
   }, []);
+
+  const handleStockChange = (event, index) => {
+    const updatedStockAllocation = [...chosenStockAllocation];
+    updatedStockAllocation[index] = {
+      ...updatedStockAllocation[index],
+      stock: event.target.value,
+    };
+    setChosenStockAllocation(updatedStockAllocation);
+  };
+
+  const handleAllocationChange = (event, index) => {
+    const updatedStockAllocation = [...chosenStockAllocation];
+    updatedStockAllocation[index] = {
+      ...updatedStockAllocation[index],
+      allocation: event.target.value,
+    };
+    setChosenStockAllocation(updatedStockAllocation);
+  };
+
+  const removeStockField = (index) => {
+    const updatedStockAllocation = [...chosenStockAllocation];
+    updatedStockAllocation.splice(index, 1);
+    setChosenStockAllocation(updatedStockAllocation);
+  };
+
+  useEffect(() => {
+    console.log("triggered");
+    console.log(chosenStockAllocation);
+  }, [chosenStockAllocation]);
 
   return (
     <>
@@ -26,8 +60,11 @@ export default function CreatePortfolioButton() {
         aria-labelledby="createPortfolio"
         aria-hidden="true"
       >
-        <div className="modal-dialog">
-          <div className="modal-content" style={{ width: "800px" }}>
+        <div
+          className="modal-dialog"
+          style={{ width: "1200px", maxWidth: "1200px" }}
+        >
+          <div className="modal-content" style={{ width: "1200px" }}>
             <div className="modal-header">
               <h1 className="modal-title fs-5" id="createPortfolioLabel">
                 Create Portfolio
@@ -43,90 +80,165 @@ export default function CreatePortfolioButton() {
               <div className="container">
                 {/* portfolio name */}
                 <div className="row p-1">
-                  <div class="col-3">
-                    <label for="name" class="col-form-label">
+                  <div className="col-3">
+                    <label for="name" className="col-form-label">
                       Portfolio Name
                     </label>
                   </div>
-                  <div class="col-9">
-                    <input type="text" id="name" class="form-control" />
+                  <div className="col-9">
+                    <input type="text" id="name" className="form-control" />
                   </div>
                 </div>
                 {/* description */}
                 <div className="row p-1">
-                  <div class="col-3">
-                    <label for="description" class="col-form-label">
+                  <div className="col-3">
+                    <label for="description" className="col-form-label">
                       Description
                     </label>
                   </div>
-                  <div class="col-9">
+                  <div className="col-9">
                     <textarea
                       type="text"
                       id="description"
-                      class="form-control"
+                      className="form-control"
                     />
                   </div>
                 </div>
                 {/* Capital */}
                 <div className="row p-1">
-                  <div class="col-3">
-                    <label for="capital" class="col-form-label">
+                  <div className="col-3">
+                    <label for="capital" className="col-form-label">
                       Capital
                     </label>
                   </div>
-                  <div class="col-9">
-                    <input type="text" id="capital" class="form-control" />
+                  <div className="col-9">
+                    <input type="text" id="capital" className="form-control" />
                   </div>
                 </div>
-                {/* Stock */}
+                {/* timePeriod */}
                 <div className="row p-1">
-                  <div class="col-3">
-                    <label for="stock" class="col-form-label">
-                      Stock
+                  <div className="col-3">
+                    <label for="timePeriod" className="col-form-label">
+                      Time Period
                     </label>
                   </div>
-                  <div class="col-9">
+                  <div className="col-9">
+                    <select id="timePeriod" className="form-select">
+                      <option value="1">Monthly</option>
+                      <option value="2">Quarterly</option>
+                      <option value="3">Yearly</option>
+                    </select>
+                  </div>
+                </div>
+                {/* Start Date */}
+                <div className="row p-1">
+                  <div className="col-3">
+                    <label for="startDate" className="col-form-label">
+                      Start Date
+                    </label>
+                  </div>
+                  <div className="col-9">
                     <input
                       type="text"
-                      id="stock"
-                      class="form-control"
-                      onChange={(value) => {
-                        setStock(value.target.value);
-                      }}
+                      id="startDate"
+                      className="form-control"
                     />
                   </div>
                 </div>
 
-                {validStocks.includes(stock) ? (
-                  <>
-                    <div className="row p-1">
-                      <div class="col-3">
-                        <label for="price" class="col-form-label">
-                          Price
-                        </label>
+                {/* Stock allocation */}
+                <div className="mt-3">
+                  <span className="fw-bold">Asset Allocation</span>
+                  <div className="row p-1">
+                    <div className="col"></div>
+                    <div className="col">Stock Symbol</div>
+                    <div className="col">Allocation</div>
+                    <div className="col-1"></div>
+                  </div>
+                  {chosenStockAllocation.map((ele, index) => {
+                    return (
+                      <div key={ele.id} className="p-0 m-0">
+                        <div className="row p-1">
+                          <div className="col">Asset {index + 1}</div>
+                          <div className="col">
+                            <input
+                              type="text"
+                              id={`stock-${index}`}
+                              className="form-control"
+                              onChange={(event) =>
+                                handleStockChange(event, index)
+                              }
+                              placeholder={ele.stock}
+                            />
+                          </div>
+                          <div className="col">
+                            <input
+                              type="text"
+                              id={`allocation-${index}`}
+                              className="form-control"
+                              onChange={(event) => {
+                                handleAllocationChange(event, index);
+                                setTotalStockAlloc(
+                                  Number(totalStockAlloc) -
+                                    Number(ele.allocation) +
+                                    Number(event.target.value)
+                                );
+                              }}
+                              placeholder={ele.allocation}
+                            />
+                          </div>
+                          {index !== 0 ? (
+                            <button
+                              className="col-1 btn btn-secondary"
+                              // onClick={() => removeStockField(index)}
+                              onClick={() => {
+                                setChosenStockAllocation(
+                                  chosenStockAllocation.filter(
+                                    (a) => a.id != ele.id
+                                  )
+                                );
+                                setTotalStockAlloc(
+                                  Number(totalStockAlloc) -
+                                    Number(ele.allocation)
+                                );
+                              }}
+                            >
+                              X
+                            </button>
+                          ) : (
+                            <div className="col-1"></div>
+                          )}
+                        </div>
                       </div>
-                      <div class="col-9">
-                        <input type="text" id="price" class="form-control" />
-                      </div>
-                    </div>
-                    <div className="row p-1">
-                      <div class="col-3">
-                        <label for="quantity" class="col-form-label">
-                          Quantity
-                        </label>
-                      </div>
-                      <div class="col-9">
-                        <input type="text" id="quantity" class="form-control" />
-                      </div>
-                    </div>
-                  </>
-                ) : (
-                  <div></div>
-                )}
+                    );
+                  })}
+                </div>
+                <hr />
+
+                <div className="p-1 d-flex justify-content-between">
+                  <span className="fw-bold">Total</span>
+                  <span className="fw-bold w-25 text-center">
+                    {totalStockAlloc}
+                  </span>
+                </div>
+                <div className="row p-1 d-flex justify-content-end mt-4">
+                  <button
+                    className="w-25 btn btn-secondary"
+                    onClick={() => {
+                      setChosenStockAllocation([
+                        ...chosenStockAllocation,
+                        { stock: "", allocation: 0, id: numStockFields },
+                      ]);
+                      setNumStockFields(numStockFields + 1);
+                    }}
+                  >
+                    Add Stock
+                  </button>
+                </div>
               </div>
             </div>
             <div className="modal-footer d-flex justify-content-center">
-              <button type="button" className="btn btn-secondary">
+              <button type="button" className="btn btn-success">
                 Create
               </button>
             </div>
--- Frontend/src/pages/Portfolios/index.jsx
+++ Frontend/src/pages/Portfolios/index.jsx
@@ -1,16 +1,17 @@
 import React, { useEffect, useState } from "react";
 import AreaChart from "../../components/Common/AreaChart";
 import DashboardCard from "../../components/Common/DashboardCard";
-import AddStockButton from "../../components/Stocks/AddStockButton";
 import CreatePortfolioButton from "../../components/Portfolios/CreatePortfolioButton";
 
 export default function Portfolios() {
-  const [stocks, setStocks] = useState([]);
+  const [trades, setTrades] = useState([]);
+  const [userPortfolios, setUserPortfolios] = useState([]);
+  const [chosenPortfolio, setChosenPortfolio] = useState(0);
 
   useEffect(() => {
-    const getStocks = async () => {
-      // const stocksFromServer = await fetchStocks();
-      const stocksFromServer = [
+    const getUserTrades = async () => {
+      // const tradesFromServer = await fetchTrades();
+      const tradesFromServer = [
         {
           name: "AAPL",
           price: 100,
@@ -26,23 +27,71 @@ export default function Portfolios() {
           returns: 200,
         },
       ];
-      setStocks(stocksFromServer);
+      setTrades(tradesFromServer);
     };
-    getStocks();
+    getUserTrades();
+
+    // getting the user's portfolio
+    setUserPortfolios([
+      {
+        portfolioId: "1",
+        name: "Retirement",
+        description: "retirement plan",
+        totalCapital: 1000,
+        Trades: [],
+      },
+      {
+        portfolioId: "2",
+        name: "Retirement 2",
+        description: "retirement plan2",
+        totalCapital: 1000,
+        Trades: [],
+      },
+      {
+        portfolioId: "3",
+        name: "Retirement 3",
+        description: "retirement plan3",
+        totalCapital: 1000,
+        Trades: [],
+      },
+    ]);
+    setChosenPortfolio({
+      portfolioId: "1",
+      name: "Retirement",
+      description: "retirement plan",
+      totalCapital: 1000,
+      Trades: [],
+    });
   }, []);
 
   return (
     <>
       <div className="container-fluid my-2 px-4 pt-2">
-        <div className="d-flex flex-row-reverse">
-          <div className="w-25 d-flex justify-content-end">
+        <div className="row d-flex justify-content-between">
+          <select
+            className="col-2 rounded"
+            onChange={(e) =>
+              setChosenPortfolio(
+                userPortfolios.find(
+                  (portfolio) => portfolio.portfolioId === e.target.value
+                )
+              )
+            }
+          >
+            {userPortfolios.map((portfolio) => {
+              return (
+                <option value={portfolio.portfolioId}>{portfolio.name}</option>
+              );
+            })}
+          </select>
+          <div className="col-2 d-flex justify-content-end">
             <CreatePortfolioButton />
           </div>
         </div>
         <div className="row mb-3 mt-5">
           <div className="col">
-            <h3>Portfolio Name</h3>
-            <div>description strategy etc</div>
+            <h3>{chosenPortfolio.name}</h3>
+            <div>{chosenPortfolio.description}</div>
           </div>
           <div className="col text-end fw-bold">
             Current Portfolio Value:
@@ -120,20 +169,22 @@ export default function Portfolios() {
               className="row p-1 fw-medium"
               style={{ backgroundColor: "lightgray" }}
             >
-              <div className="col-5">NASDAQ</div>
+              <div className="col-3">NASDAQ</div>
               <div className="col">PRICE</div>
               <div className="col">QTY</div>
               <div className="col">CAPITAL GAINS</div>
               <div className="col">RETURNS</div>
+              <div className="col">CAPITAL ALLOCATION</div>
             </div>
-            {stocks.map((element) => {
+            {trades.map((element) => {
               return (
                 <div className="row p-1">
-                  <div className="col-5">{element.name}</div>
+                  <div className="col-3">{element.name}</div>
                   <div className="col">{element.price}</div>
                   <div className="col">{element.quantity}</div>
                   <div className="col">{element.capitalGain}</div>
                   <div className="col">{element.returns}</div>
+                  <div className="col">100%</div>
                 </div>
               );
             })}
