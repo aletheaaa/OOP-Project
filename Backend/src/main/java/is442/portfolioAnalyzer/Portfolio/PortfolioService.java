685f4ae02069aafd1a1ec50a5b3f3c7491cbfc7c
zwyeo <kyzeyeo@Kyzes-MacBook-Air.local>
2023-11-07 21:13:50 +0800

    Fix sharpeRatio

--- Backend/src/main/java/is442/portfolioAnalyzer/Portfolio/PortfolioService.java
+++ Backend/src/main/java/is442/portfolioAnalyzer/Portfolio/PortfolioService.java
@@ -575,7 +575,10 @@ public class PortfolioService {
         int currentYearValue = Year.now().getValue();
         int startYearValue = Integer.parseInt(portfolio.getStartDate().substring(0, 4)); // get the year e.g. 2005
         int timePeriod = currentYearValue - startYearValue;
-        double CAGR = Math.pow(finalBalance / initialBalance, 1.0 / timePeriod) - 1;
+        if (timePeriod == 0) {
+            return 0;
+        }
+        double CAGR = Math.pow(finalBalance / initialBalance, (1.0 / timePeriod)) - 1;
         return CAGR * 100; // Convert to percentage
     }
 
@@ -588,7 +591,7 @@ public class PortfolioService {
         double riskFreeRate = 4.57;
         double expectedReturn = getPortfolioExpectedReturns(portfolioId);
         double standardDeviation = getPortfolioStandardDeviation(portfolioId);
-        return (expectedReturn - riskFreeRate) / standardDeviation;
+        return ((expectedReturn - riskFreeRate) / standardDeviation) * 100;
     }
 
     // Get Portfolio's Standard Deviation ---------------------------------------------------------------------------
